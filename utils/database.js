06787d1efbe2b5f073f1fa87dd3635b7f3982ed5
Dimfacion <arangalad@gmail.com>
2020-08-30 15:43:03 +0200

    Update database.js

--- utils/database.js
+++ utils/database.js
@@ -2,20 +2,8 @@ var database = {};
 const uri = "mongodb://localhost/baldr-db"
 const mongoose = require('mongoose');
 
-database.init = function(config, callback) {
-  mongoose.connect(uri, {
-    useNewUrlParser: true
-  });
-  const db = mongoose.connection;
-  db.on('error', function() {
-    console.log("Error while connecting to database")
-    callback(false);
-  });
-  db.once('open', function() {
-    // we're connected!
-    console.log("Connected to database")
-    callback(true);
-  });
+database.init = function(config, callback) {  
+  callback(true);
 }
 
 module.exports = database;
